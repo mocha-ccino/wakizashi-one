4bc6cf58c71cf2ba7af4e9861a94816a2272424c
mocha-ccino <59441009+mocha-ccino@users.noreply.github.com>
2022-06-13 14:59:02 +0100

    Updated main.py
    Added movement functions and gravity

--- main.py
+++ main.py
@@ -16,20 +16,69 @@ gameDisplay = pygame.display.set_mode(
     (WINDOW_WIDTH,
     WINDOW_HEIGHT)
     )
-pygame.display.set_caption("Wakizashi One")
+pygame.display.set_caption("WAKIZASHI-ONE")
+icon = pygame.image.load("src/icon.png")
+pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-# Making some global colors to reference later.
+# Making some global variables to reference later, like common colors and also speeds.
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+MAX_SPEED = 20
 
 player = Player(
-    (WINDOW_WIDTH/2) - 10,
-    (WINDOW_HEIGHT/2) - 5,
+    (WINDOW_WIDTH/2),
+    (WINDOW_HEIGHT/2),
     20,
-    10,
+    40,
     RED)
+
+def gravity(player, windowHeight):
+
+    if player.y + player.height >= windowHeight:
+        player.Vy = 0
+
+    else:
+        if player.Vy < 30:
+            player.Vy += 2
+
+def isGrounded(player):
+    if player.y + player.height >= WINDOW_HEIGHT:
+        isJumped = False
+
+def movement(player, WINDOW_WIDTH):
+
+    isJumped = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        if player.x <= 0:
+            player.x = 0
+
+        if player.Vx > -MAX_SPEED:
+            player.Vx -= 2
+
+    if keys[pygame.K_RIGHT]:
+        if player.x + player.width >= WINDOW_WIDTH:
+            player.x = WINDOW_WIDTH - player.width
+        if player.Vx < MAX_SPEED:
+            player.Vx += 2
+
+    if keys[pygame.K_SPACE]:
+        if isJumped == False:
+            isJumped = True
+            player.Vy = -10
+
+    if keys[pygame.K_DOWN]:
+        pass
+
+    if player.Vx > 0:
+        player.Vx -= 1.5
+    
+    elif player.Vx < 0:
+        player.Vx += 1.5
 
 crashed = False
 
@@ -45,8 +94,15 @@ def gameLoop():
     gameDisplay.fill(BLACK)
     player.draw(gameDisplay)
 
+    gravity(player, WINDOW_HEIGHT)
+    movement(player, WINDOW_WIDTH)
+    player.update()
+
+    if player.y >= WINDOW_HEIGHT:
+        player.y = WINDOW_HEIGHT - player.height - 2
+        player.Vy = 0
+
     
-    #player.update()
     clock.tick(FPS)
 
 
--- playerClass.py
+++ playerClass.py
@@ -2,7 +2,7 @@ import pygame
 
 class Player():
 
-    def __init__(self, x, y, height, width, color):
+    def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
         self.width = width
